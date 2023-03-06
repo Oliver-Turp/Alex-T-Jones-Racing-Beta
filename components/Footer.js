91f277f2ae394bd87823e78e7d91e284d9a32594
unknown <oliver_turp@hotmail.co.uk>
2023-03-06 12:37:35 +0000

    Footer Skeleton Done

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,48 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareFacebook,
+  faInstagram,
+  faYoutube,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <section className="footer-row">
+        <section className="footer-copyRight">&copy; Alex Jones - 2022</section>
+        <section className="footer-links">
+          <Link href="">Privacy Policy</Link>
+          <Link href="">Sitemap</Link>
+          <Link href="">Page</Link>
+          <Link href="">Page</Link>
+        </section>
+        <section className="footer-socials">
+          <Link href="#">
+            <FontAwesomeIcon icon={faSquareFacebook} />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faInstagram} />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faYoutube} />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faTwitter} />
+          </Link>
+        </section>
+      </section>
+      <section className="footer-row">
+        <p>
+          Website By{" "}
+          <Link className="footer-OT" href="#">
+            Oliver Turp
+          </Link>
+        </p>
+      </section>
+    </footer>
   );
 };
 
